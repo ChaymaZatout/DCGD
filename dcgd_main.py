002f2978a689f558df3bdbc252e44fb51ddfb9d2
chim <zatout.chayma@gmail.com>
2021-01-30 18:34:14 +0100

    DCGD main.

--- dcgd_main.py
+++ dcgd_main.py
@@ -2,25 +2,9 @@
 Author : Chayma Zatout
 Execution time optimized by : Racha Salhi
 """
-import cv2
-import freenect
 from Components.DCGD import *
 from Components.Visualizer import *
-
-
-# function to get RGB image from kinect:
-def get_rgb():
-    array, _ = freenect.sync_get_video()
-    array = cv2.cvtColor(array, cv2.COLOR_RGB2BGR)
-
-    return array
-
-
-# function to get depth image from kinect:
-def get_depth():
-    array = freenect.sync_get_depth()[0]
-    array = array.astype(np.uint16)
-    return array
+import cv2
 
 
 # depth image rendering to draw colors:
@@ -45,34 +29,10 @@ def pretty_depth(d):
 
 
 if __name__ == '__main__':
+    results_dir = "_out/"
 
-    results_dir = ""
-    # displaying videos:
-    cv2.namedWindow('Depth')
-    cv2.namedWindow('RGB')
-
-    while 1:
-        # get a frame from RGB camera
-        rgbimg = get_rgb()
-        # display RGB image
-        cv2.imshow('RGB', rgbimg)
-
-        # get a frame from depth sensor
-        depth = get_depth()
-
-        # display depth image
-        cv2.imshow('Depth', depth)
-
-        # quit program when 'esc' key is pressed
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-            break
-    cv2.destroyAllWindows()
-
-    # save image:
-    pretty = pretty_depth(depth)
-    cv2.imwrite(results_dir + "pretty.png", pretty)
-    cv2.imwrite(results_dir + "rgb.png", rgbimg)
+    depth = cv2.imread('_in/0_d.png', cv2.CV_16U)
+    pretty = cv2.imread('_in/0_p.png')
 
     # Camera parameters:
     cy_depth = 2.3844389626620386e+02
@@ -80,44 +40,22 @@ if __name__ == '__main__':
     cam = Camera(cy_depth, fy_depth)
     interval = (800, 4000)
 
-    # ground detection:
-
     # create a DCGD object
     h_err = 20
-    size_err = 5
+    size_err = 3
     step = 15
     dcgd = DCGD(cam, interval, h_err, size_err, step)
 
-    # floor detection: ########################################################################
-    cuts, ymin = dcgd.compute_cuts_downsampling_miny(depth)
-    subcuts = dcgd.compute_all_subcuts(cuts)
-    affM = dcgd.compute_transformationMatrix(subcuts) # changement de repere
-    subcuts = dcgd.map_allsubcuts(subcuts, affM)
+    # floor detection:
+    labels, labelsn, noise, minimalFloorPoints = dcgd.cgd_process_downsampling(depth)
 
-    ############################################################################################
-    filtered, noise = dcgd.filter_all_subcuts(subcuts)
-    # labels = dcgd.label_all_subcuts(filtered)
-    # labelsn = dcgd.label_all_subcuts(subcuts)
-
-    labels = dcgd.label_all_subcuts_miny(filtered, ymin, 10)
-    labelsn = dcgd.label_all_subcuts_miny(subcuts, ymin, 10)
-    
-    # Get minimal floor points of the labels set, the ones annotated with 'cc' per depth indice
-    floorPoints_labels = [[point for a in labels[ind] if a[0] == 'cc' for point in a[1]] for ind in range(lenList)]
-    
-    # Get minimal floor points of the labelsn set, the ones annotated with 'cc' per depth indice
-    floorPoints_labelsn = [[point for a in labelsn[ind] if a[0] == 'cc' for point in a[1]] for ind in range(lenList)]
-
-    p = np.copy(pretty)
-    # visualization:
-    Visualizer.viz_all_labels(labelsn, interval, show=True, filename=results_dir +"labels.png")
-    Visualizer.viz_all_filtered_labels(labels, noise, interval, show=True, filename=results_dir +"filtered.png")
-    Visualizer.viz_on_depth_downsampling(pretty, depth, floorPoints_labels, interval, h_err, step, noise, cy_depth, fy_depth)
-    Visualizer.viz_on_depth_downsampling(p, depth, floorPoints_labelsn, interval, h_err, step, noise, cy_depth, fy_depth)
-
+    # Floor visualization
+    start = time.time()
+    floorPoints = Visualizer.viz_on_depth_downsampling(pretty, depth, minimalFloorPoints, interval,
+                                                       h_err, step, cy_depth, fy_depth)
+    end = time.time()
+    print(f'Consumed time: {end - start}')
 
     # save floor:
-    cv2.imwrite(results_dir + "floor_f.png", pretty)
+    pretty = cv2.medianBlur(pretty, 11)
     cv2.imwrite(results_dir + "floor.png", pretty)
-    cv2.imshow("Floor", pretty)
-    cv2.waitKey(0)
