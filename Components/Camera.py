d10c9158426e858fe2d5e965bddcba3176e9c5b6
chim <zatout.chayma@gmail.com>
2021-01-31 17:30:10 +0100

    cx and fx added as attributes.

--- Components/Camera.py
+++ Components/Camera.py
@@ -3,9 +3,11 @@ Author : Chayma Zatout
 """
 class Camera:
 
-    def __init__(self, cy_depth, fy_depth):
+    def __init__(self, cy_depth, fy_depth, cx_depth=0, fx_depth=0):
         self._cy_d = cy_depth
         self._fy_d = fy_depth
+        self._cx_d = cx_depth
+        self._fx_d = fx_depth
 
     @property
     def cy_d(self):
@@ -15,4 +17,11 @@ class Camera:
     def fy_d(self):
         return self._fy_d
 
+    @property
+    def fx_d(self):
+        return self._fx_d
+
+    @property
+    def cx_d(self):
+        return self._cx_d
 
